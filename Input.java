bbc0a79303fcdc99244fadfc3b3195100de7e4f2
sxtrng <sxtrng@gmail.com>
2026-05-10 07:45:38 -0400

    User Scanner class to input an integer.

---
+++ Input.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int number;
+
+        System.out.print("Enter an integer: ");
+
+        number = scanner.nextInt();
+
+        System.out.printf("You Entered: %d\n", number);
+
+
+    }
+
+}
